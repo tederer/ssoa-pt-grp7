db5bd0535dd9aefab1d12c3c7e881b1b87a93ec4
Thomas Ederer <2210781006@fh-burgenland.at>
2022-11-28 19:44:44 +0100

    started implementation of order service and added swagger to it

--- services/common/src/Version.js
+++ services/common/src/Version.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 webshop.getVersion = function getVersion() {
     var result;
     try {
-        var fileContent = fs.readFileSync('package.json', 'utf8');
+        var fileContent = fs.readFileSync(__dirname + '/../../../package.json', 'utf8');
         var packageJson = JSON.parse(fileContent);
         result = packageJson.version;
     } catch(e) {
