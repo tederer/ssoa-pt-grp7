c9c781f2920744225cdfbaf7e913e6c4f6da4779
Thomas Ederer <2210781006@fh-burgenland.at>
2022-12-05 19:39:09 +0100

    adapted startService.sh for alpine

--- startService.sh
+++ startService.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+
+# using /bin/sh because alpine docker images does not support /bin/bash
 
 cd $(dirname $0)
 scriptDir=$(pwd)
 serviceName=$1
 configFile=$scriptDir/services.cfg
 
-if [ "$serviceName" == "" ] ; then
+if test "$serviceName" = "" ; then
    echo "ERROR: Missing service name. Please provide it as first argument."
    exit 1
 fi
 
-if [ ! -e $configFile ]; then
+if test ! -e $configFile ; then
    echo "ERROR: configuration file ($configFile) does not exist."
    exit 1
 fi
@@ -19,14 +21,14 @@ cd "$scriptDir"
 
 serviceConfig=$(cat $configFile | tr -d '\r' | grep "${serviceName}=")
 exitCode=$?
-if [ $exitCode -ne 0 ]; then
+if test $exitCode -ne 0 ; then
    echo "ERROR: service \"$serviceName\" does not exist in config file"
    exit 1
 fi
 
 serviceExecutable=$(echo -n $serviceConfig | cut -d '=' -f 2)
 
-if [ ! -d ./node_modules ]; then
+if test ! -d ./node_modules ; then
    npm install
 fi
 
