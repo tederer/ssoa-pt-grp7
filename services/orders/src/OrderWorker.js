d8004e5047049c673fdbd2e1673dcca0b6675951
Thomas Ederer <2210781006@fh-burgenland.at>
2022-12-20 16:34:35 +0100

    continued implementation of OrderWorker

--- services/orders/src/OrderWorker.js
+++ services/orders/src/OrderWorker.js
@@ -12,54 +12,57 @@ require('./States.js');
 assertNamespace('webshop.orders');
 
 webshop.orders.OrderWorker = function OrderWorker(database, collectionName) {
-   const MAX_ORDER_PROCESSING_DURATION_IN_MS = 10000; // TODO take higher value
-   const SLEEP_DURATION_IN_MS                = 1000;
-   const LOGGER                              = webshop.logging.LoggingSystem.createLogger('OrderWorker');
-   const STATES                              = webshop.orders.States;
-   const HTTP_CLIENT                         = new webshop.webserver.HttpClient();
-   const RESPONSE                            = webshop.webserver.HttpResponses;
-   
+   const MAX_DURATION_OF_A_SINGLE_PROCESSING_ATTEMPT_IN_MS  = 10000; // TODO take higher value
+   const SLEEP_DURATION_IN_MS                               = 1000;
+   const LOGGER                                             = webshop.logging.LoggingSystem.createLogger('OrderWorker');
+   const STATES                                             = webshop.orders.States;
+   const HTTP_CLIENT                                        = new webshop.webserver.HttpClient();
+   const RESPONSE                                           = webshop.webserver.HttpResponses;
+
    var apiGatewayIpAddress;
 
-   var getOldestNewOrder = async function getOldestNewOrder() {
-      var oldestNewOrder = await database.getLongestUnmodified(collectionName, {state: STATES.new.toString()});
-      if (oldestNewOrder !== undefined) {
-         var orderId       = oldestNewOrder._id;
-         var newState      = STATES.inProgress.toString();
-         LOGGER.logInfo('setting state of order ' + orderId + ' to ' + newState);
-         await database.updateOne(collectionName, {_id:  orderId}, {$set: {'state': newState, lastModification: Date.now()}});
-      }
-      return oldestNewOrder;
+   var setOrderState = async function setOrderState(order, state) {
+      LOGGER.logInfo('setting state of order ' + order._id + ' to ' + state.toString());
+      await database.updateOne(collectionName, {_id:  order._id}, {$set: {'state': state.toString(), lastModification: Date.now()}});
    };
 
-   var resetStateOfExpiredOrdersInProgress = async function resetStateOfExpiredOrdersInProgress() {
-      var query = {
-         $and: [
-            {state:              STATES.inProgress.toString()},
-            {lastModification:   {$lt: Date.now() - MAX_ORDER_PROCESSING_DURATION_IN_MS}}
-         ]
-      };
+   var getApiGatewayIpAddress = async function getApiGatewayIpAddress() {
+      if (apiGatewayIpAddress === undefined) {
+         const appConfig = new webshop.AppConfiguration(process.env.APP_CONFIG_CONNECTION_STRING);
+         apiGatewayIpAddress = await appConfig.get('API_GATEWAY_IP_ADDR');
+      }
+      return apiGatewayIpAddress;
+   };
 
+   var afterTimeoutSetState = async function afterTimeoutSetState(stateOfInterest, newState, timeoutInMs) {
+      var query = {
+         $and: [  {state:              stateOfInterest.toString()},
+                  {lastModification:   {$lt: Date.now() - timeoutInMs}} ]
+      };
+      
       var modifiedCount = await database.executeAsTransaction(async function(db) {
-         return await db.updateMany(collectionName, query, {$set: {'state': STATES.new.toString(), lastModification: Date.now()}});
+         return await db.updateMany(collectionName, query, {$set: {'state': newState.toString(), lastModification: Date.now()}});
       });
 
       if (modifiedCount > 0) {
-         LOGGER.logInfo('reset state for ' + modifiedCount + ' order(s) because they expired');
+         LOGGER.logInfo('changed state from ' + stateOfInterest + ' to ' + newState + ' for ' + modifiedCount + ' order(s) because they expired');
       }
+   };
+   
+   var resetStateOfExpiredOrdersInProgress = async function resetStateOfExpiredOrdersInProgress() {
+      afterTimeoutSetState(STATES.inProgress, STATES.new, MAX_DURATION_OF_A_SINGLE_PROCESSING_ATTEMPT_IN_MS);
+   };
+
+   var resetStateOfExpiredOrdersInUndo = async function resetStateOfExpiredOrdersInUndo() {
+      afterTimeoutSetState(STATES.undo, STATES.readyForUndo, MAX_DURATION_OF_A_SINGLE_PROCESSING_ATTEMPT_IN_MS);
    };
 
    var queryProductsInCart = async function queryProductsInCart(cartContent) {
-      if (apiGatewayIpAddress === undefined) {
-         const appConfig = webshop.AppConfiguration(process.env.APP_CONFIG_CONNECTION_STRING);
-         apiGatewayIpAddress = await appConfig.get('DATABASE_CONNECTION_STRING');
-      }
-
       var productsInCart = {};
 
       for (var i = 0; i < cartContent.length; i++) {
          var content = cartContent[i];
-         var response = await HTTP_CLIENT.get(apiGatewayIpAddress, '/product/byid/' + content.productId);
+         var response = await HTTP_CLIENT.get(await getApiGatewayIpAddress(), '/product/byid/' + content.productId);
          if (response.statusCode === RESPONSE.OK) {
             productsInCart[content.productId] = response.data;
          } else {
@@ -76,31 +79,68 @@ webshop.orders.OrderWorker = function OrderWorker(database, collectionName) {
       return total;
    };
 
-   var processOrder = async function processOrder(order) {
-      LOGGER.logInfo('processing order ' + order._id.toString());
-      var productsInCart = await queryProductsInCart(order.cartContent);
-      console.log(productsInCart);
-      var orderTotal     = calculateOrderTotal(order.cartContent, productsInCart);
-      console.log('total = ' + orderTotal);
-      // TODO implement order processing here. Transaction necessary?
-      
-      var newState = STATES.approved.toString();
-      LOGGER.logInfo('setting state of order ' + order._id.toString() + ' to ' + newState);
-      await database.updateOne(collectionName, {_id:  order._id}, {$set: {'state': newState, lastModification: Date.now()}});
+   var getNextNewOrder = async function getNextNewOrder() {
+      return database.executeAsTransaction(async function(db) {
+         var order = await db.getLongestUnmodified(collectionName, {state: STATES.new.toString()});
+         if (order !== undefined) {
+            setOrderState(order, STATES.inProgress);
+         }
+         return order;
+      });
    };
 
-   var processNextOrder = async function processNextOrder() {
-      try {
-         var oldestNewOrder;
-         while ((oldestNewOrder = await database.executeAsTransaction(getOldestNewOrder)) !== undefined) {
-            await processOrder(oldestNewOrder);
+   var assertValidResponse = function assertValidResponse(response, description) {
+      if (response.statusCode !== RESPONSE.OK) {
+         throw 'failed to ' + description + ': (HTTP status code=' + response.statusCode + ')';
+      };
+   };
+
+   var progressNewOrders = async function progressNewOrders() {
+      var order = await getNextNewOrder();
+      
+      while (order !== undefined) {
+         LOGGER.logInfo('processing order ' + order._id);
+         var products       = await queryProductsInCart(order.cartContent);
+         var orderTotal     = calculateOrderTotal(order.cartContent, products);
+         var idempotencyKey = order._id.toString();
+         
+         var response = await HTTP_CLIENT.post(await getApiGatewayIpAddress(), '/customer/credit', {
+            idempotencyKey: idempotencyKey,
+            customerId:     order.customerId,
+            increment:      -orderTotal
+         });
+         assertValidResponse(response, 'decrement customer credit');
+         
+         for (var i = 0; i < order.cartContent.length; i++) {
+            var content = order.cartContent[i];
+            response = await HTTP_CLIENT.post(await getApiGatewayIpAddress(), '/product/quantity/', {
+               idempotencyKey: idempotencyKey,
+               productId:      content.productId,
+               increment:      -content.quantity
+            });
+            assertValidResponse(response, 'decrement product quantity');
          }
-         resetStateOfExpiredOrdersInProgress();
+         
+         setOrderState(order, STATES.approved);
+         order = await getNextNewOrder();
+      }
+   };
+
+   var progressReadyForUndoOrders = async function progressReadyForUndoOrders() {
+      // TODO
+   };
+
+   var startNextIteration = async function startNextIteration() {
+      try {
+         await progressNewOrders();
+         await resetStateOfExpiredOrdersInProgress();
+         await progressReadyForUndoOrders();
+         await resetStateOfExpiredOrdersInUndo();
       } catch(error) {
          LOGGER.logError(error);
       }
-      setTimeout(processNextOrder, SLEEP_DURATION_IN_MS);
+      setTimeout(startNextIteration, SLEEP_DURATION_IN_MS);
    };
 
-   processNextOrder();
+   startNextIteration();
 };
--- services/orders/src/States.js
+++ services/orders/src/States.js
@@ -4,6 +4,15 @@ require('../../common/src/NamespaceUtils.js');
 
 assertNamespace('webshop.orders');
 
+// States (in upper case) and their transitions (in camel case) of an order:
+// 
+//  NEW ── workerTakesIt ──> IN_PROGRESS ─── creditOk & productsAvailable ──> APPROVED
+//   ^                           │  │    
+//   │                           │  │  ┌──── notEnoughCredit ─────────┐
+//   └──────── workerTimeout ────┘  └──┤                              ├─────> READY_FOR_UNDO ── workerTakesIt ──> UNDO ──> REJECTED
+//                                     └──── productNotAvailable ─────┘              ^                             │ 
+//                                                                                   └───────── workerTimeout ─────┘ 
+
 var State = function State(name) {
    this.toString = function toString() {
      return name;
@@ -11,8 +20,10 @@ var State = function State(name) {
 };
  
 webshop.orders.States = {
-   new:        new State('NEW'),
-   inProgress: new State('IN_PROGRESS'),
-   approved:   new State('APPROVED'),
-   rejected:   new State('REJECTED')
+   new:           new State('NEW'),
+   inProgress:    new State('IN_PROGRESS'),
+   approved:      new State('APPROVED'),
+   readyForUndo:  new State('READY_FOR_UNDO'),
+   undo:          new State('UNDO'),
+   rejected:      new State('REJECTED')
 };
