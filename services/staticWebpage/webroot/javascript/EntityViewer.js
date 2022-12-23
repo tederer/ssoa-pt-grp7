4fd8e640b921fea37dc011addff4574fa90b2f5d
Thomas Ederer <2210781006@fh-burgenland.at>
2022-12-23 17:15:58 +0100

    implemented real-time views

---
+++ services/staticWebpage/webroot/javascript/EntityViewer.js
@@ -0,0 +1,94 @@
+var EntityViewer = function EntityViewer(settings) {
+
+   const BASE_URL                = '';  // use something like "http://13.69.50.179" for debugging
+   const REFRESH_INTERVAL_IN_MS  = 1000;
+   
+   const entityName              = settings.entityName;
+   const descriptionId           = settings.decriptionId;
+   const tableHeadId             = settings.tableHeadId;
+   const tableBodyId             = settings.tableBodyId;
+   const lastModificationId      = settings.lastModificationId;
+   const columnNames             = settings.columnNames;
+   
+   var lastModificationInUse;
+   var lastEntityIdsInUse = [];
+
+   var httpGet = async function httpGet(path) {
+      return new Promise((resolve, reject) => {
+         try {
+            $.get(path, data => resolve( data ));
+         } catch(e) {
+            reject(e);
+         }
+      });
+   };
+
+   var refreshTableBody = function refreshTableBody(entities) {
+      var bodyHtml = '';
+      entities.forEach(entity => {
+         bodyHtml += '<tr>';
+         columnNames.forEach(name => {
+            bodyHtml += '<td>' + entity[name] + '</td>\n';
+         });
+         bodyHtml += '</tr>';
+      });
+      bodyHtml += '</tr>';
+      $('#' + tableBodyId).html(bodyHtml);
+   };
+
+   var initializeDocumentTitle = function initializeDocumentTitle() {
+      $('title').text(entityName);
+   };
+
+   var initializeTableHead = function initializeTableHead() {
+      var headHtml = '<tr class="table-dark">';
+      columnNames.forEach(name => {
+         headHtml += '<th scope="col">' + name + '</th>';
+      });
+      headHtml += '</tr>';
+      $('#' + tableHeadId).html(headHtml);
+   };
+
+   var getEntities = async function getEntities() {
+      var ids      = await httpGet(BASE_URL + '/' + entityName);
+      var entities = {lastModification: 0, data: []};
+      
+      for (var id of ids) {
+         var entity                = await httpGet(BASE_URL + '/' + entityName + '/byid/' + id);
+         entities.lastModification = Math.max(entity.lastModification, entities.lastModification);
+         entities.data.push(entity);
+      }
+      return entities;
+   };
+
+   var refreshRequired = function refreshRequired(entities) {
+      var entityIds                 = entities.data.map(e => e._id).sort();
+      var entityIdsDiffer           = (lastEntityIdsInUse.length !== entityIds.length) || (lastEntityIdsInUse.join() != entityIds.join());
+      var lastModificationDiffers   = lastModificationInUse !== entities.lastModification;
+      var entityChanged             = false;
+
+      if (entityIdsDiffer || lastModificationDiffers) {
+         lastModificationInUse = entities.lastModification;
+         lastEntityIdsInUse    = entityIds;
+         entityChanged         = true;
+      }
+      return entityChanged;
+   };
+
+   var refreshData = async function refreshData() {
+      var entities = await getEntities();
+      if (refreshRequired(entities)) {
+         refreshTableBody(entities.data);
+      }
+      var timestamp = (entities.lastModification > 0) ? (new Date(entities.lastModification)).toLocaleString() : 'n.a.';
+      $('#' + lastModificationId).text(timestamp);
+      setTimeout(refreshData, REFRESH_INTERVAL_IN_MS);
+   };
+
+   window.onload = async function() {
+      initializeDocumentTitle();
+      initializeTableHead();
+      $('#' + descriptionId).text(entityName);
+      refreshData();
+   };
+};
